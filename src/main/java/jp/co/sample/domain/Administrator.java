fcca476ba015d30035f95cc71e480e92202ed522
tomookakeita <keita516tomo@outlook.jp>
2019-05-28 10:28:30 +0900

    Employeeドメインクラスの実装

--- src/main/java/jp/co/sample/domain/Administrator.java
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -1,41 +1,65 @@
 package jp.co.sample.domain;
 
 public class Administrator {
+
+	/** ID */
 	private Integer id;
+	/** 名前 */
 	private String name;
-	private String mailaddress;
+	/** メールアドレス */
+	private String mailAddress;
+	/** パスワード */
 	private String password;
-	
+
+	public Administrator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Administrator(Integer id, String name, String mailAddress, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mailAddress = mailAddress;
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
-		return "Administrator [id=" + id + ", name=" + name + ", mailaddress=" + mailaddress + ", password=" + password
+		return "Administrator [id=" + id + ", name=" + name + ", mailaddress=" + mailAddress + ", password=" + password
 				+ "]";
-		
+
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMailaddress() {
-		return mailaddress;
+		return mailAddress;
 	}
+
 	public void setMailaddress(String mailaddress) {
-		this.mailaddress = mailaddress;
+		this.mailAddress = mailaddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
 }
